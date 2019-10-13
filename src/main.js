8578abd704f4d8ef3a3855192664d2f030be28b1
chinawanghsilin <1178458767@qq.com>
2019-10-13 19:01:33 +0800

    准备资源

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import './style/index.less'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 
